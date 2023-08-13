6b2940b1d49923114caab107fee5a080bb6de770
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2023-08-13 10:28:39 +0530

    added: update and delete prompt

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -3,9 +3,13 @@
 import { useState } from 'react'
 import Image from 'next/image'
 import { useSession } from 'next-auth/react'
-import { userPathname, userRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
+
   const [copied, setCopied] = useState('')
 
   const handleCopy = () => {
@@ -53,26 +57,27 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
 
 export default PromptCard
-
-/*
-div className="prompt_card">
-      <div className="flex justify-between item-start gap-5">
-        <div>
-          <Image
-            src={post.creator.image}
-            alt="user_image"
-            width={40}
-            height={40}
-            className="rounded-full object-contain"
-          />
-        </div>
-      </div>
-    </div>
-*/
